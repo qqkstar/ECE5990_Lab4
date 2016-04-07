84662274318c4c3d3e24c7cc4672ef2658f546e0
qqkstar <xz254@cornell.edu>
2016-04-07 17:48:56 -0400

    wrote python square wave

--- square_wave.py
+++ square_wave.py
@@ -4,10 +4,15 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
-p = GPIO.PWM(17, 50)
+#Stability happens around 80Hz with 1% deviation
+p = GPIO.PWM(17, 80)
 p.start(0)
 
+x=0
+
 p.ChangeDutyCycle(50)
-time.sleep(3)
+while 1:
+  x=x+1
+
 p.stop()
 GPIO.cleanup()
